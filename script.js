8144818c789bdcf158d704586dbad0f8ebc31714
Nippun Tyagi <nippuntyagi@gmail.com>
2020-10-05 14:03:36 +0530

    fixed ui for landing page and first page

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(":)");
+function enterSite(){
+   $('#landing').css("transform", "translate(-200vw)");
+   $('#dimmed-bg').css({
+     "visibility": "visible",
+     "transform": "translate(0)"
+   });
+
+   $("#above-fold").css({
+     "opacity": 1
+   });
+
+   $("#full-site").css({
+     "visibility": "visible",
+     "overflow": "auto",
+     "max-height": "auto"
+   });
+}
